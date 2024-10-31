f75b6f3ec5a530354b8517f22d62997eeaa63bd1
HHHy_27 <77572125+Jy-27@users.noreply.github.com>
2024-11-01 07:27:27 +0900

    Create kline_data_download.py
    신규 업로드.

---
+++ kline_data_download.py
@@ -0,0 +1,49 @@
+import os
+import asyncio
+import datetime
+import utils
+import pickle
+from TradingDataManager import FuturesDataControl, SpotDataControl
+
+PARENT_DIR = "/Users/cjupit/Desktop"
+
+MAIN_FOLDER_NAME = "DataStore"
+NESTED_FOLDER_NAME = "KlineData"  # 오타 수정
+
+folder_path = os.path.join(PARENT_DIR, MAIN_FOLDER_NAME, NESTED_FOLDER_NAME)
+
+def create_folder(path: str):  # 함수명 오타 수정
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    else:
+        return
+
+def pickle_dump(file_name: str, data: list):
+    create_folder(folder_path)
+    path_full = os.path.join(folder_path, file_name)
+    with open(path_full, "wb") as file:
+        pickle.dump(data, file)
+    return
+
+if __name__ == "__main__":
+    obj = FuturesDataControl()
+    intervals = [interval for interval in obj.KLINE_INTERVALS[:-4]]
+    tickers = ["btcusdt", "xrpusdt", "ethusdt"]
+
+    end_date = utils._convert_to_datetime("2024-10-30 23:59:59")
+    start_date = utils._convert_to_datetime("2024-8-1 00:00:00")
+
+    for ticker in tickers:
+        data = {}  # 각 티커마다 초기화
+        for interval in intervals:
+            data[interval] = asyncio.run(
+                obj.fetch_historical_kline_hour_min(
+                    symbol=ticker,
+                    interval=interval,
+                    start_date=start_date,
+                    end_date=end_date,
+                )
+            )
+        print(f"{ticker} 수신 완료")
+        pickle_dump(file_name=f"{ticker}.pkl", data=data)
+    print("END")
